fa95824321970379fa9cfdb3f553a0da9771e360
SavageLM <nagol.egavas@gmail.com>
2024-07-11 12:10:14 -0500

    FINALLY fixed rebalance to work like its suppose to

--- red_black_tree/2-rb_tree_insert.c
+++ red_black_tree/2-rb_tree_insert.c
@@ -1,8 +1,8 @@
 #include "rb_trees.h"
 
-rb_tree_t *rotate_left(rb_tree_t *tree);
-rb_tree_t *rotate_right(rb_tree_t *tree);
-void rebalance(rb_tree_t *node);
+rb_tree_t *rotate_left(rb_tree_t *node, rb_tree_t **tree);
+rb_tree_t *rotate_right(rb_tree_t *node, rb_tree_t **tree);
+void rebalance(rb_tree_t *node, rb_tree_t **tree);
 
 
 /**
@@ -25,8 +25,8 @@ rb_tree_t *rb_tree_insert(rb_tree_t **tree, int value)
 		if (!(*tree)->left)
 		{
 			new_node = (rb_tree_node(*tree, value, 0));
-			(*tree)->left = new_node;
-			rebalance(new_node);
+			(*tree)->left = new_node; /* rebalance(new_node, tree); */
+			return (new_node);
 		}
 		else
 			new_node = rb_tree_insert(&(*tree)->left, value);
@@ -36,58 +36,79 @@ rb_tree_t *rb_tree_insert(rb_tree_t **tree, int value)
 		if (!(*tree)->right)
 		{
 			new_node = (rb_tree_node(*tree, value, 0));
-			(*tree)->right = new_node;
-			rebalance(new_node);
+			(*tree)->right = new_node; /* rebalance(new_node, tree); */
+			return (new_node);
 		}
 		else
 			new_node = rb_tree_insert(&(*tree)->right, value);
 	}
 	else
 		return (NULL);
-	rebalance(*tree);
+	if (!new_node)
+		return (NULL);
+	rebalance(new_node, tree);
 	return (new_node);
 }
 
 /**
  * rotate_left - performs a left-rotation on binary tree
- * @tree: root node of tree to be left-rotated
+ * @node: root node of tree to be left-rotated
+ * @tree: Root of entire tree
  * Return: pointer to new root node of tree following left-rotation
  */
-rb_tree_t *rotate_left(rb_tree_t *tree)
+rb_tree_t *rotate_left(rb_tree_t *node, rb_tree_t **tree)
 {
 	rb_tree_t *tmp = NULL;
 
-	if (!tree)
+	if (!node)
 		return (NULL);
-	tmp = tree->right;
-	tmp->parent = tree->parent;
-	tree->right = tmp->left;
+	tmp = node->right;
+	node->right = tmp->left;
 	if (tmp->left)
-		tmp->left->parent = tree;
-	tmp->left = tree;
-	tree->parent = tmp;
+		tmp->left->parent = node;
+	tmp->parent = node->parent;
+	if (tmp->parent)
+	{	
+		if (node == tmp->parent->left)
+			tmp->parent->left = tmp;
+		else
+			tmp->parent->right = tmp;
+	}
+	else
+		*tree = tmp;
+	tmp->left = node;
+	node->parent = tmp;
 	return (tmp);
 }
 
 /**
  * rotate_right - performs a right-rotation on binary tree
- * @tree: root node of tree to be right-rotated
+ * @node: root node of tree to be right-rotated
+ * @tree: root of entire tree
  * Return: pointer to new root node of tree following right-rotation
  */
-rb_tree_t *rotate_right(rb_tree_t *tree)
+rb_tree_t *rotate_right(rb_tree_t *node, rb_tree_t **tree)
 {
 	rb_tree_t *tmp = NULL;
 
-	if (!tree)
+	if (!node)
 		return (NULL);
-	tmp = tree->left;
-	tmp->parent = tree->parent;
-	tree->left = tmp->right;
+	tmp = node->left;
+	node->left = tmp->right;
 	if (tmp->right)
-		tmp->right->parent = tree;
-	tmp->right = tree;
-	tree->parent = tmp;
-
+		tmp->right->parent = node;
+	tmp->parent = node->parent;
+	if (tmp->parent)
+	{	
+		if (node == tmp->parent->left)
+			tmp->parent->left = tmp;
+		else
+			tmp->parent->right = tmp;
+	}
+	else
+		*tree = tmp;
+	tmp->right = node;
+	node->parent = tmp;
 	return (tmp);
 }
 
@@ -95,53 +116,36 @@ rb_tree_t *rotate_right(rb_tree_t *tree)
  * rebalance - Checks if a color change or rotation is needed
  * @node: node to check balance of
  */
-void rebalance(rb_tree_t *node)
+void rebalance(rb_tree_t *node, rb_tree_t **tree)
 {
 	rb_tree_t *uncle = NULL;
 
 	if (!PARENT || PARENT->color == 1)/*PARENT IS BLACK CASE*/
 		return;
-
+	if (!GRANDPA)
+		return;
 	uncle = GRANDPA->left == PARENT ? GRANDPA->right : GRANDPA->left;
 	if (uncle && uncle->color == 0)/*UNCLE IS RED CASE*/
+		PARENT->color = 1, uncle->color = 1, GRANDPA->color = 0;
+	else/*  if (!uncle || uncle->color == 1) *//*UNCLE IS BLACK CASE*/
 	{
-		PARENT->color = 1, uncle->color = 1;
-		if (GRANDPA->parent)
-			GRANDPA->color = 0;
-	}
-	else if (!uncle || uncle->color == 1)/*UNCLE IS BLACK CASE*/
-	{
-		if (GRANDPA->left == uncle && PARENT->right == node)
-		{/*LEFT ROTATE CASE*/
-			rotate_left(GRANDPA);
-			if (GRANDPA)
-				GRANDPA->color = 0;
-			PARENT->color = 1;
+		if (GRANDPA->left == uncle && PARENT->right == node)/*parent right node right*/
+			GRANDPA->color = 0, PARENT->color = 1, rotate_left(GRANDPA, tree);
+		else if (GRANDPA->right == uncle && PARENT->left == node)/*Parent left, node left*/
+			GRANDPA->color = 0, PARENT->color = 1, rotate_right(GRANDPA, tree);
+		else if (GRANDPA->left == uncle && PARENT->left == node)/*Parent right, node left*/
+		{
+			node = node->parent, rotate_right(node, tree);
+			PARENT->color = 1, GRANDPA->color = 0;
+			rotate_left(GRANDPA, tree);
 		}
-		else if (GRANDPA->right == uncle && PARENT->left == node)
-		{/*RIGHT ROTATE CASE*/
-			node = rotate_right(GRANDPA);
-			if (GRANDPA)
-				GRANDPA->color = 0;
-			PARENT->color = 1;
-		}
-		else if (GRANDPA->left == uncle && PARENT->left == node)
-		{/*LEFT-RIGHT ROTATE CASE*/
-			PARENT = rotate_right(PARENT);
-			/* node->left->parent = node; */
-			rotate_right(GRANDPA);
-			if (GRANDPA)
-				GRANDPA->color = 0;
-			PARENT->color = 1;
-		}
-		else
-		{/*RIGHT-LEFT ROTATE CASE*/
-			PARENT = rotate_left(PARENT);
-			/* node->right->parent = node; */
-			node = rotate_right(GRANDPA);
-			if (GRANDPA)
-				GRANDPA->color = 0;
-			PARENT->color = 1;
+		else /*Parent left, node right*/
+		{
+			node = node->parent, rotate_left(node, tree);
+			PARENT->color = 1, GRANDPA->color = 0;
+			rotate_right(GRANDPA, tree);
 		}
 	}
+	if (!(*tree)->parent)
+		(*tree)->color = 1;
 }
