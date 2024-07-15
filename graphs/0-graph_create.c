3e41d5b6e6d24d70ca4c62f15097bf35807e2bb6
SavageLM <nagol.egavas@gmail.com>
2024-07-15 12:38:34 -0500

    Fixed 0-graph

--- graphs/0-graph_create.c
+++ graphs/0-graph_create.c
@@ -2,10 +2,12 @@
 
 /**
  * graph_create - Creates a graph and initializes elements
+ * Return: created graph or NULL
  */
 graph_t *graph_create(void)
 {
 	graph_t *new_graph = NULL;
 
 	new_graph = calloc(1, sizeof(graph_t));
+	return(!new_graph ? NULL : new_graph);
 }
