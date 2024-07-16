1199fb9bb9e58383eff6a96be30263bac8f4ffb0
SavageLM <nagol.egavas@gmail.com>
2024-07-16 11:23:10 -0500

    Added code for 2-add_edges and 3-graph_delete

---
+++ graphs/2-graph_add_edge.c
@@ -0,0 +1,58 @@
+#include "graphs.h"
+
+int assign_edge(vertex_t *start, vertex_t *end);
+
+/**
+ * graph_add_edge - Addes an Edge to existing graph
+ * @graph: Graph to add edge to
+ * @src: Starting point of Edge
+ * @dest: End point of Edge
+ * @type: Edge type
+ * Return: 1 on success, else 0
+ */
+int graph_add_edge(graph_t *graph, const char *src, const char *dest, edge_type_t type)
+{
+	vertex_t *start = NULL, *end = NULL, *tmp;
+
+	if (!graph || !graph->vertices || !src || !dest)
+		return (0);
+	for (tmp = graph->vertices; tmp; tmp = tmp->next)
+	{
+		if (!strcmp(src, tmp->content))
+			start = tmp;
+		else if (!strcmp(dest, tmp->content))
+			end = tmp;
+	}
+	if (!start || !end)
+		return (0);
+	if (!assign_edge(start, end))
+		return (0);
+	if (type == 1)
+		return (assign_edge(end, start) ? 1 : 0);
+	return (1);
+}
+
+/**
+ * assign_edge - creates and assigns an edge
+ * @start: vertice to add an edge to
+ * @end: destination of edge
+ * Return: 1 on success, 0 on fail
+ */
+int assign_edge(vertex_t *start, vertex_t *end)
+{
+	edge_t *new_edge = NULL, *edge;
+
+	new_edge = malloc(sizeof(edge_t));
+	if (!new_edge)
+		return (0);
+	new_edge->dest = end, new_edge->next = NULL;
+	if (!start->edges)
+		start->edges = new_edge;
+	else
+	{
+		for (edge = start->edges; edge->next; edge = edge->next)
+			continue;
+		edge->next = new_edge, start->nb_edges += 1;
+	}
+	return (1);
+}
---
+++ graphs/3-graph_delete.c
@@ -0,0 +1,31 @@
+#include "graphs.h"
+
+/**
+ * graph_delete - Deletes a graph
+ * @graph: graph to delete
+ */
+void graph_delete(graph_t *graph)
+{
+	vertex_t *tmp_vert, *del_vert;
+	edge_t *tmp_edge, *del_edge;
+
+	if (!graph)
+		return;
+	if (!graph->vertices)
+		free(graph);
+	tmp_vert = graph->vertices;
+	while (tmp_vert)
+	{
+		tmp_edge = tmp_vert->edges;
+		while (tmp_edge)
+		{
+			del_edge = tmp_edge;
+			tmp_edge = tmp_edge->next;
+			del_edge->dest = NULL, free(del_edge);
+		}
+		del_vert = tmp_vert;
+		tmp_vert = tmp_vert->next;
+		free(del_vert->content), del_vert->edges = NULL, free(del_vert);
+	}
+	free(graph);
+}
