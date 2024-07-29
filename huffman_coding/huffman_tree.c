570546ecd7fe03998ae81b912271d0feded41f5d
SavageLM <nagol.egavas@gmail.com>
2024-07-29 16:33:19 -0500

    Added code to huffman_tree.c

---
+++ huffman_coding/huffman_tree.c
@@ -0,0 +1,28 @@
+#include "huffman.h"
+#include "heap.h"
+
+/**
+ * huffman_tree - Builds a huffman tree from a priority queue
+ * @data: Array holding the data for the symbol
+ * @freq: Array holding the frequencies of the symbols
+ * @size: size of the data and freq arrays
+ * Return: Root node of huffman tree
+ */
+binary_tree_node_t *huffman_tree(char *data, size_t *freq, size_t size)
+{
+	heap_t *prio_que = NULL;
+	btn *node = NULL;
+
+	if (!data || !freq || !size)
+		return (NULL);
+	prio_que = huffman_priority_queue(data, freq, size);
+	if (!prio_que)
+		return (NULL);
+	while (prio_que->size > 1)
+	{
+		if (!huffman_extract_and_insert(prio_que))
+			return (NULL);
+	}
+	node = heap_extract(prio_que);
+	return (node);
+}
