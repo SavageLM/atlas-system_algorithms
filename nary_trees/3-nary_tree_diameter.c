9fb75feb00524fa52a51f6f92a4ee4da58d505b4
SavageLM <nagol.egavas@gmail.com>
2024-08-12 14:38:18 -0500

    Refactored code to use two functions to work around an off by 1 error

--- nary_trees/3-nary_tree_diameter.c
+++ nary_trees/3-nary_tree_diameter.c
@@ -1,5 +1,7 @@
 #include "nary_trees.h"
 
+size_t get_diam(nt const *root);
+
 /**
  * nary_tree_diameter - Finds diameter of tree
  * @root: Tree to find diameter of
@@ -7,32 +9,33 @@
  */
 size_t nary_tree_diameter(nary_tree_t const *root)
 {
-	size_t max = 1, diam = 0;
-	
+	size_t diam = 0;
+
 	if (!root)
 		return (0);
-	diam = root->children ? nary_tree_diameter(root->children) + 1 : 0;
-	if (diam > max)
-		max = diam;
-	diam = root->next ? nary_tree_diameter(root->next) + 1 : 0;
-	if (diam > max)
-		max = diam;
-	return (max);
+	if (root->children || root->next)
+	{
+		diam = get_diam(root), diam -= 1;
+		return (diam);
+	}
+	else
+		return (1);
 }
 
 /**
  * get_diam - gets the diameter of a tree
  * @root: tree to get diam of
- * @diam: diameter
  * Return: Diameter or 0
  */
-/* size_t get_diam(nt const *root, size_t diam)
+size_t get_diam(nt const *root)
 {
-	size_t max1 = 0, max2 = 0;
+	size_t max = 0, diam = 0;
 
-	if (!root)
-		return (diam);
-	max1 = get_diam(root->children, diam + 1);
-	max2 = get_diam(root->next, diam + 1);
-	return (diam);
-} */
+	diam = root->children ? get_diam(root->children) + 1 : 0;
+	if (diam > max)
+		max = diam;
+	diam = root->next ? get_diam(root->next) + 1 : 0;
+	if (diam > max)
+		max = diam;
+	return (max);
+} 
